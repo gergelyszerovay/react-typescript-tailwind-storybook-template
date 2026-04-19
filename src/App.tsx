e9a8ea59a01d3d14c8b28ad9ed63f5b103ce3104
Gergely Szerovay <gergely@szerovay.hu>
2026-04-19 18:43:00 +0000

    0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      React + TypeScript 6 + Tailwind 4 + Storybook 10 Template
+    </h1>
+  );
 }
 
 export default App;
